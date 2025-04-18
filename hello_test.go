e7dd4ab03a14707fbf9d5e1be65ef2395b73dbc8
Leon Delaimy <leondel.coding@gmail.com>
2025-04-18 17:28:29 +0100

    default world

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T){
-	got := Hello("Leon")
-	want := "Hello, Leon!"
+	
+	t.Run("saying hello to people", func(t *testing.T){
+		got := Hello("Leon")
+		want := "Hello, Leon!"
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
 
+	t.Run("say hello world when supplied empty string", func(t *testing.T){
+		got := Hello("")
+		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string){
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
