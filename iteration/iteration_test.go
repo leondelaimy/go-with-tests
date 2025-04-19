071ca64da04bb286570e17e6964cc8dba4637eea
Leon Delaimy <leondel.coding@gmail.com>
2025-04-19 12:34:42 +0100

    iteration

---
+++ iteration/iteration_test.go
@@ -0,0 +1,37 @@
+package iteration
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func TestStdLibRepeat(t *testing.T) {
+	repeated := strings.Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for b.Loop() {
+		Repeat("a", 5)		
+	}
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("a", 5)
+	fmt.Println(repeat)
+	// Output: aaaaa
+}
